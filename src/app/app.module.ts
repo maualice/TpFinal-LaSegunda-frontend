e9c663fbbadb7bd362456d91e57ca1f490e2aac6
maualice <alicemauroe@gmail.com>
2022-09-12 17:31:01 -0300

    adding snackbar for store abm and using weather API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ComponentsModule } from './shared/modules/components/components.module';
 import { HttpClientModule } from '@angular/common/http';
+import { WeatherWidgetMainComponent } from './shared/components/weather-widget-main/weather-widget-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherWidgetMainComponent
   ],
   imports: [
     BrowserModule,

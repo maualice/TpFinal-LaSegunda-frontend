91a25909c03fd54bf59b5cd0242d35c0cb173ad9
FranG218 <franjg3@outlook.com>
2022-09-13 08:24:14 -0300

    login validation

--- src/app/modules/auth/views/login/login.component.ts
+++ src/app/modules/auth/views/login/login.component.ts
@@ -9,10 +9,10 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  
+
   user = new FormGroup({
     email: new FormControl('', { validators: [Validators.email, Validators.required] }),
-    password: new FormControl('', { validators: [Validators.required, Validators.required] }),
+    password: new FormControl('', { validators: [Validators.required] }),
   });
 
   constructor(
@@ -21,17 +21,20 @@ export class LoginComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {}
-
+messageError:string='';
   signIn() {
     if (this.user.valid) {
     this.authService.signIn(this.user.value)
       .subscribe(
         res => {
-          console.log(res)
+          console.log(res);
+          //this.authService.isAdmin(res.admin)
           localStorage.setItem('token', res.token);
           this.router.navigate(['/dashboard']);
         },
-        err => console.log(err)
+        err => {console.log(err);
+                this.messageError = '*Correo electrónico o contraseña incorrectos'
+              }
       )
     } else {
       this.user.markAllAsTouched();

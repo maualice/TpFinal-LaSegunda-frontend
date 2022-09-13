6210700f02e2eecc02d06d832b789276c1cb4ceb
FranG218 <franjg3@outlook.com>
2022-09-13 09:09:14 -0300

    admin access created

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RegisterComponent } from '../../modules/auth/views/register/register.component'; //
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import {tap} from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +15,27 @@ export class AuthService {
 
   constructor(private http: HttpClient, private router:Router) { }
 
+ 
+  
+  useradmin!: boolean;
+  admin!:string;
+
   signUp(user: { name: string; email: string; password: string; }) {
     return this.http.post<any>(this.URL + '/auth/register', user);
   }
 
   signIn(user: { email: string; password: string; }) {
-    return this.http.post<any>(this.URL + '/auth/login', user);
+    this.admin = user.email;
+    return this.http.post<any>(this.URL + '/auth/login', user)/*.pipe(tap((response:any)=> {
+      this.useradmin = response.user; 
+    }))*/
   }
+  /*
+  getOneStore(id:string): Observable <Store> {
+    return this.http.get(`${this.apiURL}/stores/`+ id).pipe(map((response:any)=> {
+      const store:Store = response.store;
+      return store;
+    })) */
 
   loggedIn(): boolean {
     return !!localStorage.getItem('token');
@@ -30,8 +47,26 @@ export class AuthService {
 
   logout() {
     localStorage.removeItem('token');
+    this.admin = ''
     //this.router.navigate(['/login'])
   }
 
 
+/*
+  signIn(user) {
+    return this.http.get<any>(this.URL + '/auth/admin', user);
+*/
+  isAdmin(){
+    if (this.admin == 'admin@mail.com') return true;
+    else return false;
+  }
+  /*
+  isAdmin(){
+      console.log(this.useradmin);
+      if (this.useradmin == true) {
+        return true;
+      }
+      else return false;
+  }
+*/
 }
